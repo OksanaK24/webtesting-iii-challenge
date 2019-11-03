7a7cccb09f697633d2e5dc01af2e061fe0d2e738
Oksana Kril <oksanakril@Oksanas-iMac.local>
2019-11-03 12:48:16 -0600

    Controls component - done; MVP - done

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -6,6 +6,56 @@ import { exportAllDeclaration } from "@babel/types";
 
 afterEach(rtl.cleanup);
 
+test("Close Gate button is exist and can be toggled", () => {
+    const toggleClosed = jest.fn()
+    const wrapper = rtl.render(<Controls toggleClosed={toggleClosed}/>)
+    const CloseGateBut = wrapper.getByText(/close gate/i)
+    expect(CloseGateBut).toExist;
+
+    rtl.act(() => {
+        rtl.fireEvent.click(CloseGateBut);
+      })
+    expect(toggleClosed).toHaveBeenCalled;
+
+
+})
+
+test("Open Gate button is exist", () =>{
+    const wrapper = rtl.render(<Controls closed={true}/>)
+    const OpenGateBut = wrapper.getByText(/open gate/i)
+    expect(OpenGateBut).toExist
+})
+
+test("Lock Gate button is exist and can be toggled", () => {
+    const toggleLocked = jest.fn()
+    const wrapper = rtl.render(<Controls toggleLocked={toggleLocked}/>)
+    const LockGateBut = wrapper.getByText(/lock gate/i)
+    expect(LockGateBut).toExist;
+
+    rtl.act(() => {
+        rtl.fireEvent.click(LockGateBut);
+      })
+    expect(toggleLocked).toHaveBeenCalled;
+})
+
+test("Unlock Gate button is exist", () =>{
+    const wrapper = rtl.render(<Controls locked={true}/>)
+    const UnlockGateBut = wrapper.getByText(/unlock gate/i)
+    expect(UnlockGateBut).toExist
+})
+
+test("Lock Gate button is disabled when close gates are open", () => {
+    const wrapper = rtl.render(<Controls />)
+    const LockGateButDis = wrapper.getByText(/lock gate/i)
+    expect(LockGateButDis).toBeDisabled
+})
+
+test("Open Gate button is disabled when gates are locked", () => {
+    const wrapper = rtl.render(<Controls locked={true} closed ={true} />)
+    const OpenGateButDis = wrapper.getByText(/open gate/i)
+    expect(OpenGateButDis).toBeDisabled
+})
+
 // test ("cannot be closed or opened if it is locked", () => {
 
 //     const isLocked = jest.fn()
