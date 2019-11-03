82a6076ea3efe51f11c61cdaa570abead47d6a8e
Oksana Kril <oksanakril@Oksanas-iMac.local>
2019-11-03 12:05:59 -0600

    display component - done

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -18,3 +18,27 @@ test("defaults to open", () => {
     const defaultOpen = wrapper.getByText(/open/i)
     expect(defaultOpen).toBeTruthy();
 })
+
+test("displays 'Closed' if the closed prop is true + closed use the red-led class", () =>{
+    const wrapper = rtl.render(<Display closed={true}/>)
+    const ClosedGate = wrapper.getByText(/closed/i)
+    expect(ClosedGate).toHaveClass("led red-led")
+})
+
+test("displays 'Open' if the closed prop is false(default) + open use the green-led class", () =>{
+    const wrapper = rtl.render(<Display />)
+    const OpenGate = wrapper.getByText(/open/i)
+    expect(OpenGate).toHaveClass("led green-led")
+})
+
+test("displays 'Locked' if the locked prop is true + locked use the red-led class", () =>{
+    const wrapper = rtl.render(<Display locked={true}/>)
+    const LockedGate = wrapper.getByText(/locked/i)
+    expect(LockedGate).toHaveClass("led red-led")
+})
+
+test("displays 'Unlocked' if the locked prop is false(default) + open use the green-led class", () =>{
+    const wrapper = rtl.render(<Display />)
+    const UnlockedGate = wrapper.getByText(/unlocked/i)
+    expect(UnlockedGate).toHaveClass("led green-led")
+})
